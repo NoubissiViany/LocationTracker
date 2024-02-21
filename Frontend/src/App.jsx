917493724acf68e2a0731835f24a8f7842a36674
NNVG <g696607013@gmail.com>
2024-02-21 10:29:16 +0100

    first commit

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div>
-      <p className="text-3xl font-bold my-56 text-center">Welcome!</p>
+      <p className="text-3xl font-bold my-56 text-center">Welcome to Location Tracker!</p>
     </div>
   );
 }
