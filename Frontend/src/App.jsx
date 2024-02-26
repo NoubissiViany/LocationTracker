529c7650aeecfe75ce42871d9539b895bf2da7f0
NNVG <g696607013@gmail.com>
2024-02-26 11:54:28 +0100

    update registration and login design

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,15 @@
-function App() {
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./Login";
+import Register from "./Register";
+
+export default function App() {
   return (
-  <></>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Login />} />
+        <Route path="/registration" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
-
-export default App;
